dc2beb8ac342ff9831c3625289f72b4ae3af3a99
DGwebdes <alan.bilik8@gmail.com>
2025-01-22 02:15:44 +0000

    added login/logout button to nav bar 1.2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import LoginNavButton from './LoginNavButton';
 import '../styles/navBar.css'
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <h1>Dielan's Task Manager</h1>
+            <h1>Dielan&apos;s Task Manager</h1>
             <LoginNavButton />
         </nav>
     )
