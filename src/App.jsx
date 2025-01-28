9b9ab631ab247598eece26f851722543d4b862f5
DGwebdes <alan.bilik8@gmail.com>
2025-01-28 02:05:47 +0000

    fixed urls for imgs

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
     <Navbar />
-      <Router>
+      <Router basename='/task-manager-app'>
         <ErrorBoundary>
           <Routes>
             <Route path="/" element={<HomePage />} />
