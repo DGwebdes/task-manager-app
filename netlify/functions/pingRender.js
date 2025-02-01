e1f68b7e7001f3adc6dcf62615af2a66565d97ac
DGwebdes <alan.bilik8@gmail.com>
2025-02-01 17:32:25 +0000

    remove axios from netlify function, replace with fetch.then

--- netlify/functions/pingRender.js
+++ netlify/functions/pingRender.js
@@ -1,20 +1,20 @@
-import axios from 'axios';
-
-export const handler = async () => {
-  const url ='https://task-manager-server-3i2w.onrender.com';
+export async function handler() {
+  const url = 'https://task-manager-server-3i2w.onrender.com';
 
   try {
-    const response = await axios.get(url, { timeout: 5000 });
-    console.log(`Ping response status: ${response.status}`);
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Server ping failed with status: ${response.status}`);
+    }
+
     return {
       statusCode: 200,
       body: 'Server pinged successfully',
     };
   } catch (error) {
-    console.error(`Ping failed: ${error.message}`);
     return {
       statusCode: 500,
       body: `Server ping failed: ${error.message}`,
     };
   }
-};
+}
