8ccacdceff92916c4683db3566b7361e9f2a683b
DGwebdes <alan.bilik8@gmail.com>
2025-04-25 02:04:20 +0100

    updating dependencies

--- netlify/functions/pingRender.js
+++ netlify/functions/pingRender.js
@@ -1,20 +1,20 @@
-export async function handler() {
-  const url = 'https://task-manager-server-3i2w.onrender.com';
+// export async function handler() {
+//   const url = 'https://task-manager-server-3i2w.onrender.com';
 
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Server ping failed with status: ${response.status}`);
-    }
+//   try {
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//       throw new Error(`Server ping failed with status: ${response.status}`);
+//     }
 
-    return {
-      statusCode: 200,
-      body: 'Server pinged successfully',
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: `Server ping failed: ${error.message}`,
-    };
-  }
-}
+//     return {
+//       statusCode: 200,
+//       body: 'Server pinged successfully',
+//     };
+//   } catch (error) {
+//     return {
+//       statusCode: 500,
+//       body: `Server ping failed: ${error.message}`,
+//     };
+//   }
+// }
